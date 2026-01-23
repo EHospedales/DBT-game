95a8ee3764395f1008878966e726bdf0b84be5a8
EHospedales <emilio92796@gmail.com>
2026-01-23 18:57:20 +0000

    forced

--- app/dbt-game/host/page.tsx
+++ app/dbt-game/host/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const dynamic = "force-dynamic"
+
 import { QRCodeSVG } from "qrcode.react"
 import { useEffect, useState } from "react"
 import { supabase } from "../lib/supabase"
